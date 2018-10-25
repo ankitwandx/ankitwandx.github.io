8ddd571320bd4cb51db8bdf95b24e35be883b1ef
bssrdev <bssrinivasarao@gmail.com>
2018-10-26 02:44:11 +0530

    Extended the time

--- js/popUpCounter.js
+++ js/popUpCounter.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Oct 25, 2018 14:00:00").getTime();
+var countDownDate = new Date("Oct 26, 2018 11:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
